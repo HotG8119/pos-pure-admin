59038c5042650fe7be603f9d58c1bcfb5f12c09d
HotG8119 <k508060@gmail.com>
2024-05-16 12:24:49 +0800

    feat: test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -23,6 +23,8 @@ import Lock from "@iconify-icons/ri/lock-fill";
 import Check from "@iconify-icons/ep/check";
 import User from "@iconify-icons/ri/user-3-fill";
 
+const { VITE_APP_BACKEND_URL } = import.meta.env;
+
 defineOptions({
   name: "Login"
 });
@@ -93,6 +95,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
+  {{ VITE_APP_BACKEND_URL }}
   <div class="select-none">
     <img :src="bg" class="wave" />
     <div class="flex-c absolute right-5 top-3">
